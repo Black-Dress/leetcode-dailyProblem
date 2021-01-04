053f3cdbd8937ac5ae6fb8c2675ad85831217f16
blackdress <lcjemail@qq.com>
2021-01-04 11:10:59 +0800

    2021/01/04

---
+++ January.py
@@ -0,0 +1,14 @@
+class Solution:
+    # 509
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        res = [0 for i in range(n+1)]
+        res[0], res[1] = 0, 1
+        for i in range(2, n+1):
+            res[i] = res[i-1]+res[i-2]
+        return res[-1]
+
+
+s = Solution()
+print(s.fib(3))
