2ed76689c091a703e7ccb045d88f54adf7d82b87
blackdress <lcjemail@qq.com>
2021-01-05 12:19:55 +0800

    2021/01/05

--- January.py
+++ January.py
@@ -9,6 +9,21 @@ class Solution:
             res[i] = res[i-1]+res[i-2]
         return res[-1]
 
+    # 830
+    def largeGroupPositions(self, s: str) -> [[int]]:
+        res = list()
+        index = 1
+        for i in range(1, len(s)):
+            if s[i] == s[i-1]:
+                index += 1
+            else:
+                index = 1
+            if index == 3:
+                res.append([i-2, i])
+            if index > 3:
+                res[-1][1] = i
+        return res
+
 
 s = Solution()
-print(s.fib(3))
+print(s.largeGroupPositions("acd"))
