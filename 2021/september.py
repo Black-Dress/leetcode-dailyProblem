7ca31fff3a1e0fc02d87804ac1082621179e9007
black-dress <lcjemail@qq.com>
2021-09-14 10:29:46 +0800

    # 524. 通过删除字母匹配到字典里最长单词

--- 2021/september.py
+++ 2021/september.py
@@ -17,6 +17,32 @@ class Solution:
                 res += v*(v-1)
         return res
 
+    # 524. 通过删除字母匹配到字典里最长单词
+    def findLongestWord(self, s: str, dictionary: List[str]) -> str:
+        # 先判断 s 是否含有字典中的某一个值
+        # 只要源字符串包含了匹配串的所有字符，并且按照匹配串的顺序 则成功匹配
+        # 双指针匹配
+        def match(s: str, ss: str) -> bool:
+            i, j, p, q = 0, s.__len__()-1, 0, ss.__len__()-1
+            while i <= j:
+                while s[i] != ss[p] and i < j:
+                    i += 1
+                p += 1 if s[i] == ss[p] else 0
+                i += 1
+                while s[j] != ss[q] and i < j:
+                    j -= 1
+                q -= 1 if s[j] == ss[q] else 0
+                j -= 1
+                if p > q:
+                    return True
+            return False
+        dictionary.sort()
+        dictionary.sort(key=lambda x: len(x), reverse=True)
+        for i in dictionary:
+            if match(s, i):
+                return i
+        return ""
+
 
 s = Solution()
-print(s.numberOfBoomerangs([[0, 0], [1, 0], [2, 0]]))
+print(s.findLongestWord("abcdefgc", ["acc", "acbcc", "accc", "abc"]))
