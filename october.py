4962816aa63cd80b3ff841a99a9ce7975c7f5f9d
black-dress <lcjemail@qq.com>
2021-10-11 09:56:00 +0800

    # 273. 整数转换英文表示

--- october.py
+++ october.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import Collection, List
+from typing import Collection, List, no_type_check
 
 
 class Solution:
@@ -16,6 +16,39 @@ class Solution:
                 res.append(k)
         return res
 
+    # 273. 整数转换英文表示
+    def numberToWords(self, num: int) -> str:
+        singles = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+        teens = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+        tens = ["", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+        thousands = ["", "Thousand", "Million", "Billion"]
+        if num == 0:
+            return "Zero"
+
+        def num_helper(num: int) -> str:
+            s = ""
+            if num == 0:
+                return s
+            elif num < 10:
+                s += singles[num] + " "
+            elif num < 20:
+                s += teens[num - 10] + " "
+            elif num < 100:
+                s += tens[num // 10] + " " + num_helper(num % 10)
+            else:
+                s += singles[num // 100] + " " + "Hundred " + num_helper(num % 100)
+            return s
+
+        res, unit = "", int(1e9)
+        for i in range(3, -1, -1):
+            curNum = num // unit
+            # unit 代表一个三位数字的元组，如果curNum不为零则代表进入到正常阶段
+            if curNum:
+                num -= curNum * unit
+                res += num_helper(curNum) + thousands[i] + " "
+            unit //= 1000
+        return res.strip()
+
 
 s = Solution()
-print(s.findRepeatedDnaSequences("AAAAAAAAAAAAA"))
+print(s.numberToWords(12345))
