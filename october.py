455316480453058bbc6c3ca0996b28ba2fd482f2
black-dress <lcjemail@qq.com>
2021-10-29 11:22:56 +0800

    # 335. 路径交叉

--- october.py
+++ october.py
@@ -191,6 +191,27 @@ class Solution:
         table = {countDigit(1 << i) for i in range(30)}
         return countDigit(n) in table
 
+    # 335. 路径交叉
+    def isSelfCrossing(self, distance: List[int]) -> bool:
+        n = len(distance)
+        for i in range(3, n):
+            # 第 1 类路径交叉的情况
+            if (distance[i] >= distance[i - 2]
+                    and distance[i - 1] <= distance[i - 3]):
+                return True
+
+            # 第 2 类路径交叉的情况
+            if i == 4 and (distance[3] == distance[1]
+                           and distance[4] >= distance[2] - distance[0]):
+                return True
+
+            # 第 3 类路径交叉的情况
+            if i >= 5 and (distance[i - 3] - distance[i - 5] <= distance[i - 1] <= distance[i - 3]
+                           and distance[i] >= distance[i - 2] - distance[i - 4]
+                           and distance[i - 2] > distance[i - 4]):
+                return True
+        return False
+
 
 s = Solution()
-print(s.reorderedPowerOf2(1))
+print(s.isSelfCrossing([1, 1, 2, 2, 3, 3, 4, 4, 10, 4, 4, 3, 3, 2, 2, 1, 1]))
