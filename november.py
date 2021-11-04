37d3dfec3d8f5b16c9298b11a15a7f0d93f02631
black-dress <lcjemail@qq.com>
2021-11-04 09:44:37 +0800

    # 367. 有效的完全平方数

--- november.py
+++ november.py
@@ -13,3 +13,17 @@ class Solution:
                 table[i] += 1
                 cnt += 1
         return table.__len__()
+
+    # 367. 有效的完全平方数
+    def isPerfectSquare(self, num: int) -> bool:
+        l, r = 0, num
+        while l <= r:
+            mid = (l + r) // 2
+            square = mid * mid
+            if square < num:
+                l = mid + 1
+            elif square > num:
+                r = mid - 1
+            else:
+                return True
+        return False
