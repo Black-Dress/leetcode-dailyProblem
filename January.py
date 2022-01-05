0538c85e4f475a4ec256dfa0daec6317187f3586
black-dress <lcjemail@qq.com>
2022-01-05 17:53:46 +0800

    # 33. 搜索旋转排序数组

--- January.py
+++ January.py
@@ -51,25 +51,25 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # 确定 target和nums[0]的大小关系，确定target在左半区还是右半区
         l, r = 0, len(nums) - 1
-        while l <= r:
-            mid = (l + r) >> 1
-            if nums[mid] == target:
-                return mid
-            # 如果在左半区
-            if nums[0] <= target < nums[mid]:
-                r = mid - 1
-            if nums[0] <= nums[mid] < target:
-                l = mid + 1
-            if nums[mid] < nums[0] and target >= nums[0]:
-                r = mid - 1
-            # 如果在右半区
-            if nums[mid] < target < nums[0]:
-                l = mid + 1
-            if target < nums[mid] < nums[0]:
-                r = mid - 1
-            if nums[mid] >= nums[0] and target < nums[0]:
-                l = mid + 1
-        return -1
+        # while l <= r:
+        #     mid = (l + r) >> 1
+        #     if nums[mid] == target:
+        #         return mid
+        #     # 如果在左半区
+        #     if nums[0] <= target < nums[mid]:
+        #         r = mid - 1
+        #     if nums[0] <= nums[mid] < target:
+        #         l = mid + 1
+        #     if nums[mid] < nums[0] and target >= nums[0]:
+        #         r = mid - 1
+        #     # 如果在右半区
+        #     if nums[mid] < target < nums[0]:
+        #         l = mid + 1
+        #     if target < nums[mid] < nums[0]:
+        #         r = mid - 1
+        #     if nums[mid] >= nums[0] and target < nums[0]:
+        #         l = mid + 1
+        # return -1
 
 
 s = Solution()
