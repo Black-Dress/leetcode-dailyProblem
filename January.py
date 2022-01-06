b00624b1a103136968e790edfc3de1a50edd399a
black-dress <lcjemail@qq.com>
2022-01-06 15:28:56 +0800

    # 40. 组合总和 II

--- January.py
+++ January.py
@@ -1,5 +1,5 @@
 import collections
-from typing import Collection, Dict, List
+from typing import Collection, Dict, List, Literal
 from NodeHelper.ListNode import ListNode
 
 
@@ -116,28 +116,50 @@ class Solution:
         # 题目要求不能选取重复数字，并且不能有重复结果
         # 需要记录 cur 的状态，如果 cur 状态相同那么就不需要再进入循环了
         # cur 第一个位置存储数组之和
+        # def DFS(candiates: List[int], target: int, index: int, cur: List[int], res: List[List[int]]):
+        #     # cur 状态已经走过，return
+        #     if cnt.get("".join(str(i) for i in cur[1:])) is not None:
+        #         return
+        #     if cur[0] == target:
+        #         res.append(cur[1:].copy())
+        #         return
+        #     for i in range(index, len(candidates)):
+        #         if cur[0] < target:
+        #             cur[0] += candidates[i]
+        #             cur.append(candidates[i])
+        #             # 从下一位开始
+        #             DFS(candiates, target, i + 1, cur, res)
+        #             # 进入一次循环之后更新，状态
+        #             cnt["".join(str(j) for j in cur[1:])] = 1
+        #             cur[0] -= candidates[i]
+        #             cur.pop()
+        #         if cur[0] > target:
+        #             return
+
+        # candidates.sort()
+        # res, cnt = [], collections.defaultdict(str)
+        # DFS(candidates, target, 0, [0], res)
+        # return res
+
         def DFS(candiates: List[int], target: int, index: int, cur: List[int], res: List[List[int]]):
-            # cur 状态已经走过，return
-            if cnt.get("".join(str(i) for i in cur[1:])) is not None:
-                return
             if cur[0] == target:
                 res.append(cur[1:].copy())
                 return
-            for i in range(index, len(candidates)):
+            for i in range(index, candiates.__len__()):
+                if i > index and candiates[i] == candiates[i - 1]:
+                    continue
                 if cur[0] < target:
                     cur[0] += candidates[i]
                     cur.append(candidates[i])
                     # 从下一位开始
                     DFS(candiates, target, i + 1, cur, res)
                     # 进入一次循环之后更新，状态
-                    cnt["".join(str(j) for j in cur[1:])] = 1
                     cur[0] -= candidates[i]
                     cur.pop()
-                if cur[0] > target:
-                    return
-
+                else:
+                    break
         candidates.sort()
-        res, cnt = [], collections.defaultdict(str)
+        res = []
         DFS(candidates, target, 0, [0], res)
         return res
 
@@ -166,4 +188,4 @@ s = Solution()
 a = ListNode.createListNode([1, 4, 5])
 b = ListNode.createListNode([1, 3, 4])
 c = ListNode.createListNode([2, 6])
-print(s.combinationSum2([2, 5, 2, 1, 2], 5))
+print(s.combinationSum2([10, 1, 2, 7, 6, 1, 5], 8))
