676f003c477f360cf49ef5e2e9caf424f035587f
black-dress <lcjemail@qq.com>
2022-01-20 14:47:43 +0800

    # 剑指 Offer 11. 旋转数组的最小数字

--- January.py
+++ January.py
@@ -759,20 +759,26 @@ class Solution:
             return False
         return binarySerach(matrix, [0, 0, len(matrix) - 1, len(matrix[0]) - 1], target)
 
+    # 剑指 Offer 11. 旋转数组的最小数字
+    def minArray(self, numbers: List[int]) -> int:
+        n = len(numbers)
+        l, r = 0, n - 1
+        while l <= r:
+            mid = (l + r) >> 1
+            if numbers[mid] < numbers[r]:
+                r = mid
+            elif numbers[mid] == numbers[r]:
+                r -= 1
+            else:
+                l = mid + 1
+        return numbers[l]
+
 
 s = Solution()
 a = ListNode.createListNode([1, 4, 5])
 b = ListNode.createListNode([1, 3, 4])
 c = ListNode.createListNode([2, 6])
-print(s.findNumberIn2DArray(
-    [
-        [1, 2, 3, 4, 5],
-        [6, 7, 8, 9, 10],
-        [11, 12, 13, 14, 15],
-        [16, 17, 18, 19, 20],
-        [21, 22, 23, 24, 25]
-    ],
-    5))
+print(s.minArray([3, 4, 5, 1, 2]))
 # [5,1,5,5,2,5,4]
 # [2,5,0,6,6]
 # [2,5,0,1,2]
