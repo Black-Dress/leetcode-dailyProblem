df4c84bffb425910906ac324a8394d81d849da7e
black-dress <lcjemail@qq.com>
2022-02-14 15:37:35 +0800

    # 540. 有序数组中的单一元素

--- February.py
+++ February.py
@@ -56,6 +56,45 @@ class Solution:
         total = sum(dp[n][:])
         return sorted([dp[n][i] / total for i in range(n, n * 6 + 1)])
 
+    # 剑指 Offer 14- II. 剪绳子 II
+    def cuttingRope(self, n: int) -> int:
+        if n <= 3:
+            return n - 1
+        a, b, mod, res, x = n // 3 - 1, n % 3, 1000000007, 1, 3
+        # 快速幂求余
+        while a:
+            if a % 2:
+                res = (res * x) % mod
+            x = (x**2) % mod
+            a //= 2
+        if b == 0:
+            return (res * 3) % mod
+        if b == 1:
+            return (res * 4) % mod
+        return (res * 6) % mod
+
+    # 540. 有序数组中的单一元素
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        def dfs(l: int, r: int, nums: List[int]) -> int:
+            if l > r:
+                return -1
+            mid = (l + r) >> 1
+            # 和前面相等
+            a = mid > 0 and nums[mid] == nums[mid - 1]
+            # 和后面相等
+            b = mid < r and nums[mid] == nums[mid + 1]
+            # 奇偶
+            c = (mid + 1) % 2 == 0
+            if not a and not b:
+                return nums[mid]
+            if (a and c) or (b and not c):
+                return dfs(mid + 1, r, nums)
+            if (b and c) or (a and not c):
+                return dfs(l, mid - 1, nums)
+        return dfs(0, len(nums) - 1, nums)
+
 
 s = Solution()
-print(s.dicesProbability(2))
+print(s.singleNonDuplicate([3, 3, 7, 7, 10, 11, 11]))
+# print(sorted([1, 2, 3, 4, 5, 6], key=lambda x: (x == 1, x - 1)))
+# print(min([1, 2, 3, 4, 5, 6], key=lambda x: (x == 1, x - 1)))
