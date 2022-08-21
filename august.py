03a5dfd985a8da80a85e9301c3a2a777a128787d
liuchenjun <liuchenjun.bb@bytedance.com>
2022-08-21 20:18:29 +0800

    网易笔试

--- august.py
+++ august.py
@@ -1,5 +1,8 @@
+from collections import defaultdict
 from curses.ascii import isdigit
+from email.policy import default
 from operator import eq
+from sys import stdin
 
 
 class Solution:
@@ -35,6 +38,66 @@ class Solution:
             return "No solution" if val else "Infinite solutions"
         return f"x={-val // factor}"
 
+    def solution1():
+        a, b = stdin.readline().split(" ")
+        # 大数是小数的倍数，大数可以删除数字小数可以删除数字
+        # 最少的操作次数
+        # 如何删除？永远删除大数的数字？之后再删除小数的数字？
+
+    def solution4(self):
+        n = int(stdin.readline())
+        nums = list(map(int, stdin.readline().split(" ")))
+        cnt = defaultdict(list)
+        res = 0
+
+        for i in range(n):
+            cnt[nums[i]].append(i)
+        for v in cnt.values():
+            if len(v) >= 2:
+                index = []
+                for i in range(1, len(v)):
+                    index.append(0)
+                    for j in range(v[i - 1], v[i]):
+                        if nums[j] < nums[v[0]]:
+                            index[-1] += 1
+                for i in range(len(index)):
+                    res += (len(index) - i) * index[i]
+                    if i != 0 and i != len(index) - 1:
+                        res += (len(index) - i) * index[i]
+        print(res)
+
+    def solution3(self):
+        n = int(stdin.readline())
+        nums = list(map(int, stdin.readline().split(" ")))
+        a, b = nums[0], nums[1]
+        sum_a, sum_b = 0, 0
+        res = 0
+        for i, val in enumerate(nums):
+            if (i + 1) % 2 == 0:
+                sum_a += val
+                if val > a:
+                    a = val
+
+            if (i + 1) % 2 != 0:
+                sum_b += val
+                if val > b:
+                    b = val
+
+        if a != b:
+            res = a * (n // 2) - sum_a + b * ((n + 1) // 2) - sum_b
+        else:
+            res = min((a + 1) * (n // 2) - sum_a + b * ((n + 1) // 2) - sum_b, a * (n // 2) - sum_a + (b + 1) * ((n + 1) // 2) - sum_b)
+        print(res)
+
+    def solution2():
+        s = stdin.readline()
+        max_num = (s // 5) * 2 + (s // 3)
+        i, res = 0, 0
+        reds = s.split("red")
+        for i in reds:
+            if len(i) >= 2 and (i[:-2] == "de" or i[:2] == "er"):
+                pass
+
 
 s = Solution()
-print(s.solveEquation("x+5-3+x=6+x-2"))
+s.solution3()
